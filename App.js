356bab106426f1da5f778ef41dcbe2900a0d33e7
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-06-14 01:32:54 +0530

    1st video complete(Namaste React)

---
+++ App.js
@@ -0,0 +1,16 @@
+const heading1 = React.createElement("h1" , {
+    id: "title",
+} , "Namaste Everyone!");
+console.log(heading1);
+
+const heading2 = React.createElement("h2" , {
+    id: "title",
+} , "heading 2!");
+
+const container = React.createElement("div" , {
+    id: "container"
+} , [heading1, heading2])
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
