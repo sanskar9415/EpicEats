d576cd07c9cd8ed24996ddb59da63262f756a6ba
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-06-14 17:49:26 +0530

    parcel

--- App.js
+++ App.js
@@ -1,16 +1,57 @@
-const heading1 = React.createElement("h1" , {
-    id: "title",
-} , "Namaste Everyone!");
-console.log(heading1);
+// # Chapter 02 - Igniting our App
 
-const heading2 = React.createElement("h2" , {
-    id: "title",
-} , "heading 2!");
+/*
+ *** Parcel Feature ***
+ * Created A Server
+ * HMR - Hot Module Replacement
+ * File Watcher algorithm - C++ (Execute File when changes occur)
+ * BUNDLING
+ * MINIFY
+ * Cleaning our Code (Example - Remove Console.log)
+ * Dev and Production Build
+ * Super Fast build algorithm
+ * Image Optimization
+ * Caching while development
+ * Compression
+ * Compatible with older version of browser
+ * HTTPS on dev [Example - npx parcel index.html --https]
+ * Port Number [Example - If port number using in localhost then it will change in port number in another project running on localhost]
+ * Consistent Hashing Algorithm
+ * Zero Config
+ * Tree shaking
+ *
+ *
+ * Transitive Dependencies
+ */
 
-const container = React.createElement("div" , {
-    id: "container"
-} , [heading1, heading2])
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+// imported react and reactdom from nodemodule folder
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const heading = React.createElement(
+    "h1",
+    {
+      id: "title",
+    },
+    "heading"
+  );
+  const heading1 = React.createElement(
+    "h1",
+    {
+      id: "title",
+    },
+    "heading1"
+  );
+  
+  const container = React.createElement(
+    "div",
+    {
+      id: "container",
+    },
+    [heading, heading1]
+  );
+  
+  // create root using createRoot
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  // passing react element inside root
+  root.render(container);
