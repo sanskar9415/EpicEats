95899d3e786568211a55007f00098bcd700887e8
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-07-15 20:32:42 +0530

    restaurant menu basic acordion and used fontawesome

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import { faHouse, faCartShopping, faPhone, faAddressCard } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Title = () => (
     <a href="/">
@@ -17,11 +19,11 @@ const Header = function (){
       <div className='flex justify-between shadow-lg  p-1 bg-gray-200 sticky top-0'>
         <Title/>
         <div className="nav-items">
-          <ul className='flex py-3 pr-6'>
-            <li><Link className='link' to = "/">Home</Link></li>
-            <li><Link className='link' to = "/about">About</Link></li>
-            <li><Link className='link' to = "/contact">Contact</Link></li>
-            <li><Link className='link' to = "/">Cart</Link></li>
+          <ul className='flex py-3 pr-6 '>
+            <li><Link className='link mx-3 text-2xl' to = "/"><FontAwesomeIcon icon={faHouse} /></Link></li>
+            <li><Link className='link mx-3 text-2xl' to = "/about"><FontAwesomeIcon icon={faAddressCard} /></Link></li>
+            <li><Link className='link mx-3 text-2xl' to = "/contact"><FontAwesomeIcon icon={faPhone} /></Link></li>
+            <li><Link className='link mx-3 text-2xl' to = "/"><FontAwesomeIcon icon={faCartShopping} /></Link></li>
           </ul>
         </div>
       </div>
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import MenuShimmer from "./MenuShimmer";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
     const { resId } = useParams();
@@ -14,15 +15,30 @@ const RestaurantMenu = () => {
         const data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`);
         const json = await data.json();
         // console.log(json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards);
-        console.log(json?.data?.cards[2]?.card?.card?.info);
-        setRestaurant(json?.data?.cards[2]?.card?.card?.info || "");
+        // setRestaurant(json?.data?.cards[2]?.card?.card?.info);
+        console.log(json?.data?.cards[2].card.card.info);
+        setRestaurant(json);
     }
+    if(restaurant === null) return <MenuShimmer/>;
 
-  return restaurant === null ? <MenuShimmer/> : (
-    <div>
-        <h1>Restaurant id : {resId}</h1>
-        <h2>Restaurant Name : {restaurant.name}</h2>
-        <h3>Area :{restaurant.locality}, {restaurant.areaName} </h3>
+    const {name, cuisines} = restaurant?.data?.cards[2]?.card?.card?.info;
+
+    const categories = restaurant?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) => 
+       c?.card?.card?.["@type"] == 'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory'
+    );
+    console.log(categories);
+
+  return  (
+    <div className="text-center mt-8">
+        <h1 className="font-bold text-2xl mb-1">{name}</h1>
+        <h3 className="text-gray-700"> {cuisines.join(", ")}</h3>
+        {categories.map((category) => {
+          return <RestaurantCategory data = {category?.card?.card}/>
+          //we can use () for implicit return
+        })}
+
+
     </div>
   )
 }
