c32a0ca8476dfc476b9330ae3a170f96afc00b1d
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-07-16 18:13:10 +0530

    itemCard items display done

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import { IMG_CDN_URL } from '../constants';
+import nonVeg from '../assets/nonVeg.png'
+import veg from '../assets/veg.png'
 
 export const ItemList = ({items}) => {
     console.log(items);
   return (
     <div>
         {items.map((item) => (
-            <div key = {item?.card?.info?.id}>{item?.card?.info.name}</div>
+            <div key = {item?.card?.info?.id} className='p-2 m-2 border-b-2 text-left flex justify-between'>
+                <div className='w-9/12 p-2'>
+                <div className='py-2'>
+                    <div>{item?.card?.info?.itemAttribute?.vegClassifier == 'VEG' ? 
+                     (<img src={veg} alt="veg" className="w-6 h-6" />): 
+                    (<img src={nonVeg} alt="Non-Veg" className="w-6 h-6" />)}
+                    </div>
+                    <div className='font-bold text-gray-600 '>{item?.card?.info.name}</div>
+                    <div className='font-bold p-1'> ₹ {item?.card?.info.price/100 || item?.card?.info?.defaultPrice/100}</div>
+                </div>
+                <p className='text-xs'>{item?.card?.info.description}</p>
+                </div>
+                <div className='w-3/12  items-center'>
+                <img 
+                className=' rounded-3xl object-cover w-full h-36'
+                src= { IMG_CDN_URL + item?.card?.info.imageId } alt="img" />
+                </div>
+            </div>
         ))}
     </div>
   )
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -5,9 +5,9 @@ import ItemList from './ItemList';
 
 export const RestaurantCategory = ({data}) => {
   return (
-    <div  className=' bg-gray-100 w-6/12 mx-auto my-4 shadow-md '>
+    <div  className=' bg-gray-100 w-6/12 mx-auto my-4 shadow-md'>
     <div className='flex justify-between p-4'>
-        <span className='mt-1'>{data.title}  ({data.itemCards.length})
+        <span className='mt-1 font-bold'>{data.title}  ({data.itemCards.length})
         </span>
         <span>
         <FontAwesomeIcon icon={faChevronDown} className='text-gray-600 mt-2' />
